a1824e344fa18acc9f9b207fe4a7cc174f1f561d
Katrin <KatrinKroin@gmail.com>
2018-02-10 10:32:58 +0200

    send balot

--- app/src/main/java/com/example/katri/lolvotingsystem/MobileCandidateList.java
+++ app/src/main/java/com/example/katri/lolvotingsystem/MobileCandidateList.java
@@ -1,13 +1,16 @@
 package com.example.katri.lolvotingsystem;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,6 +24,7 @@ public class MobileCandidateList extends AppCompatActivity implements AsyncRespo
     private ListView lv;
     String User;
     String ExistingVote;
+    String CandidateID;
     ArrayList<HashMap<String, String>> candidateList;
     public TextView data;
     User CurrentUser;
@@ -43,23 +47,22 @@ public class MobileCandidateList extends AppCompatActivity implements AsyncRespo
         cs.delegate = this;
         cs.execute();
 
-
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this,R.style.MyDialogTheme);
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(),"Yuppppp ",Toast.LENGTH_LONG).show();
-                /*
-                new AlertDialog.Builder(this)
-                        .setMessage("Are you sure?")
+                CandidateID = candidateList.get(i).get("id");
+                Toast.makeText(getApplicationContext()," "+ CandidateID,Toast.LENGTH_LONG).show();
+                builder
+
+                        .setMessage(Html.fromHtml("<font color='#e4e5e7'>        Are you sure in your choice?</font>"))
                         .setNegativeButton("No",null)
                         .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                Toast.makeText(getApplicationContext(),"Yuppppp ",Toast.LENGTH_LONG).show();
+                                SendBalot(0);
                             }
                         }).create().show();
-                //new SendRequest().execute();
-                */
             }
         });
     }
@@ -68,4 +71,12 @@ public class MobileCandidateList extends AppCompatActivity implements AsyncRespo
     public void processFinish(ArrayList<HashMap<String, String>> candidateList) {
         this.candidateList = candidateList;
     }
+
+    private void SendBalot(int flag){
+        if(flag == 0){
+            SendBalotSync balot = new SendBalotSync(this, flag ,ExistingVote ,CandidateID);
+            balot.execute();
+        }
+    }
+
 }
