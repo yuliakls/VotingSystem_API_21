d39dac54e00834c0338f6412d34cc6fbec5ccf0f
sali Weizman <salibenmocha@gmail.com>
2018-02-10 17:12:19 +0200

    sali commit to SaliBranch

--- app/src/main/java/com/example/katri/lolvotingsystem/AddUser.java
+++ app/src/main/java/com/example/katri/lolvotingsystem/AddUser.java
@@ -6,17 +6,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class AddUser extends AppCompatActivity {
+
+//
+//    private EditText UserID;
+//    private EditText Name;
+//    private EditText Email;
+//    private EditText Password;
+//    private boolean Admin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user);
 
+        //setUpAddButton();
         setUpFinishButton();
 
     }
+
+
+    public void doIt(final View arg)
+    {
+        AddUserSync aus = new AddUserSync( this);
+        aus.execute();
+    }
+
+
+//    private void setUpAddButton() {
+//
+//        Button add_btn = (Button) findViewById(R.id.add_btn);
+//
+//        add_btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//            }
+//        });
+//    }
 
     public static Intent makeIntent(Context context) {
         return new Intent(context,AddUser.class);
@@ -30,8 +60,8 @@ public class AddUser extends AppCompatActivity {
         finish_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 finish();
+
             }
         });
 
